4860f3681236c5db71908cd822e91da71f73fa72
Sabin <spradh@users.noreply.github.com>
2023-12-26 17:39:24 -0600

    Create 06 Linear regression with a categorical explanatory variable.py

---
+++ 1-Simple-Linear-Regression-Modeling/06 Linear regression with a categorical explanatory variable.py
@@ -0,0 +1,11 @@
+# Create the model, fit it
+mdl_price_vs_age = ols('price_twd_msq ~ house_age_years', data=taiwan_real_estate).fit()
+
+# Print the parameters of the fitted model
+print(mdl_price_vs_age.params)
+
+# Update the model formula to remove the intercept
+mdl_price_vs_age0 = ols("price_twd_msq ~ house_age_years + 0", data=taiwan_real_estate).fit()
+
+# Print the parameters of the fitted model
+print(mdl_price_vs_age0.params)
