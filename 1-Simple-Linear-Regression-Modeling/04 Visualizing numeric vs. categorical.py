14a7093ab23fcad12cc65651fe012b2454f99331
Sabin <spradh@users.noreply.github.com>
2023-12-26 17:35:00 -0600

    Create 04 Visualizing numeric vs. categorical.py

---
+++ 1-Simple-Linear-Regression-Modeling/04 Visualizing numeric vs. categorical.py
@@ -0,0 +1,8 @@
+# Histograms of price_twd_msq with 10 bins, split by the age of each house
+sns.displot(data=taiwan_real_estate,
+         x='price_twd_msq',
+         col='house_age_years',
+         bins=9)
+
+# Show the plot
+plt.show()
