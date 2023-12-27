03a2e4a2f72fe2900e234ac3c2a770b387e213c3
Sabin <spradh@users.noreply.github.com>
2023-12-27 10:42:38 -0600

    Create 4 Extracting model elements.py

---
+++ 2 Predictions and Model Objects/4 Extracting model elements.py
@@ -0,0 +1,15 @@
+#1
+# Print the model parameters of mdl_price_vs_conv
+print(mdl_price_vs_conv.params)
+
+#2
+# Print the fitted values of mdl_price_vs_conv
+print(mdl_price_vs_conv.fittedvalues)
+
+#3
+# Print the residuals of mdl_price_vs_conv
+print(mdl_price_vs_conv.resid)
+
+#4
+# Print a summary of mdl_price_vs_conv
+print(mdl_price_vs_conv.summary())
