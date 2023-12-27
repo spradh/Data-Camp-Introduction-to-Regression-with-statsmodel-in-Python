32d8fc2f581f0f5c46098c74c6d625abaa6b8583
Sabin <spradh@users.noreply.github.com>
2023-12-27 09:14:52 -0600

    Create 1 Predicting house prices.py

---
+++ 2 Predictions and Model Objects/1 Predicting house prices.py
@@ -0,0 +1,8 @@
+# Import numpy with alias np
+import numpy as np
+
+# Create the explanatory_data 
+explanatory_data = pd.DataFrame({'n_convenience': np.arange(0,11)})
+
+# Print it
+print(explanatory_data)
