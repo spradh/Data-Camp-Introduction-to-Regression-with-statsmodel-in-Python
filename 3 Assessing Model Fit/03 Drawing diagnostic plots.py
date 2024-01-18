d30a37b877001745afb58b34550f09e0198ba9a2
Sabin <spradh@users.noreply.github.com>
2024-01-17 22:34:26 -0600

    Create 03 Drawing diagnostic plots.py

---
+++ 3 Assessing Model Fit/03 Drawing diagnostic plots.py
@@ -0,0 +1,31 @@
+#1
+# Plot the residuals vs. fitted values
+sns.residplot(x='n_convenience', y='price_twd_msq', data=taiwan_real_estate, lowess=True)
+plt.xlabel("Fitted values")
+plt.ylabel("Residuals")
+
+# Show the plot
+plt.show()
+
+#2
+# Import qqplot
+from statsmodels.api import qqplot
+
+# Create the Q-Q plot of the residuals
+qqplot(data=mdl_price_vs_conv.resid, fit=True, line="45")
+
+# Show the plot
+plt.show()
+
+#3
+# Preprocessing steps
+model_norm_residuals = mdl_price_vs_conv.get_influence().resid_studentized_internal
+model_norm_residuals_abs_sqrt = np.sqrt(np.abs(model_norm_residuals))
+
+# Create the scale-location plot
+sns.regplot(x=mdl_price_vs_conv.fittedvalues, y=model_norm_residuals_abs_sqrt, ci=None, lowess=True)
+plt.xlabel("Fitted values")
+plt.ylabel("Sqrt of abs val of stdized residuals")
+
+# Show the plot
+plt.show()
