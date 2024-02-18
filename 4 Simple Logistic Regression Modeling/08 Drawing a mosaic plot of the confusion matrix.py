f0b821760c8708b22a8724754379880435a0cd1c
Sabin <spradh@users.noreply.github.com>
2024-02-17 23:24:47 -0600

    Create 08 Drawing a mosaic plot of the confusion matrix.py

---
+++ 4 Simple Logistic Regression Modeling/08 Drawing a mosaic plot of the confusion matrix.py
@@ -0,0 +1,12 @@
+# Import mosaic from statsmodels.graphics.mosaicplot
+from statsmodels.graphics.mosaicplot import mosaic
+
+# Calculate the confusion matrix conf_matrix
+conf_matrix = mdl_churn_vs_relationship.pred_table()
+
+# Print it
+print(conf_matrix)
+
+# Draw a mosaic plot of conf_matrix
+mosaic(conf_matrix)
+plt.show()
